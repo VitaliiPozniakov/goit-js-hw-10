5fa0f3b81214bcd9d3a0872a5f79468523cbffa7
VitaliiPozniakov <vertalen89@gmail.com>
2022-06-09 17:43:07 +0300

    1

--- src/countriesApiServise.js
+++ src/countriesApiServise.js
@@ -18,7 +18,7 @@ export default class CountriesApiService {
         }
         return r.json();
       })
-    //   .then(console.log);
+
   }
 
   get query() {
@@ -30,21 +30,3 @@ export default class CountriesApiService {
   }
 }
 
-//
-
-export function fetchCountries(name) {
-  return fetch(`${BASE_URL}/name/${name}?${searchParams}`).then(response => {
-    if (!response.ok) {
-      throw new Error(response.status);
-    }
-    return response.json();
-  });
-  // .then(data => {
-  //   console.log(data);
-  // })
-  // .catch(error => {
-  //   console.log(error);
-  // });
-}
-
-// export default {fetchCountries}
